db618117ef4759ebbcd86833eec7081802e4d0b6
yashveer08 <yashveer.bhadouria@bugendaitech.com>
2023-07-05 20:57:47 +0530

    updated App file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This is the app version 1.0
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
